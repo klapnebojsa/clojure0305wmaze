a8097ce748000eb8fa59de5aca94d9b6a4039506
klapnebojsa <klapnebojsa@gmail.com>
2015-11-12 03:21:38 +0100

    .

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure0305wmaze
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojure0305wmaze/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure0305wmaze.core-test
+  (:require [clojure.test :refer :all]
+            [clojure0305wmaze.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
